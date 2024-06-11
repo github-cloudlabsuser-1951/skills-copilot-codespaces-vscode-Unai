0f5e486b11aa025e0354e86c288dda04fbb3321b
github-cloudlabsuser-1951 <github_cloudlabsuser_1951@cloudlabsaiuser.com>
2024-06-11 14:54:06 +0000

    Copilot commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MarkdownEditor = () => {
+    const [markdown, setMarkdown] = useState('Type markdown here');
+
+    const handleMarkdownChange = (event) => {
+        setMarkdown(event.target.value);
+    };
+
+    return (
+        <div>
+            <textarea value={markdown} onChange={handleMarkdownChange} />
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+    );
+};
+
+const sentence = 'hello world';
+const reversedSentence = markdown.split(' ').reverse().join(' ');
+const capitalizedSentence = reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1);
+console.log(capitalizedSentence);
+
+
+export default MarkdownEditor;
+
